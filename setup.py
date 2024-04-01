6c070e058d7ade0d410c2b68bcd20a2fbb77c336
vineetkukreti <vineetkukreti34@gmail.com>
2024-04-01 13:17:38 +0530

    updated setup.py

--- setup.py
+++ setup.py
@@ -9,29 +9,30 @@ with open(os.path.join(base_dir, "README.md")) as f:
 setup(
   name = 'py-AutoClean',         
   packages = ['AutoClean'],   
-  version =  'v1.1.3',      
+  version =  'v1.1.1',      
   license='MIT',        
   description = 'AutoClean - Python Package for Automated Preprocessing & Cleaning of Datasets', 
   long_description=long_description,
   long_description_content_type='text/markdown',
-  author = 'Elise Landman',                  
-  author_email = 'elisejlandman@hotmail.com', 
-  url = 'https://github.com/elisemercury/AutoClean', 
-  download_url = 'https://github.com/elisemercury/AutoClean/archive/refs/tags/v1.1.3.tar.gz',
-  keywords = ['automated', 'cleaning', 'preprocessing', "autoclean"],  
+  author = 'Vineet Kukreti',                  
+  author_email = 'vineetkukreti34@gmail.com', 
+  url = 'https://github.com/vineetkukreti/sparkledata', 
+
+  keywords = ['automated', 'cleaning', 'preprocessing', "autoclean","eda","data analysis","data preprocessing",],  
   install_requires=[          
           'scikit-learn',
           'numpy',
           'pandas',
-          'loguru'
+          'loguru',
+          'ydata-profiling'
       ],
   classifiers=[
     'Development Status :: 5 - Production/Stable',   
     'Intended Audience :: Developers',      
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',    
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: 3.9',
+    # 'License :: OSI Approved :: MIT License',    
+    # 'Programming Language :: Python :: 3.7',
+    # 'Programming Language :: Python :: 3.8',
+    # 'Programming Language :: Python :: 3.9',
   ],
 )
